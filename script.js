7e2c56e4bfa7f619e97872fa6c014380e1d8fee7
karenlaureta <karenevangelistalaureta@gmail.com>
2025-12-26 13:47:09 +0800

    super duper final up for lanterns

--- script.js
+++ script.js
@@ -644,7 +644,7 @@ function createLantern() {
 /* Start lanterns */
 function startLanterns() {
   stopLanterns();
-  lanternInterval = setInterval(createLantern, 800);
+  lanternInterval = setInterval(createLantern, 500);
 }
 
 /* Stop lanterns */
