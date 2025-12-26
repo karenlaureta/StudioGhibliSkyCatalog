514066a9ac728e4098295541f654c75cbc3f6e5d
karenlaureta <karenevangelistalaureta@gmail.com>
2025-12-26 13:24:06 +0800

    UPDATE lanterns in js

--- script.js
+++ script.js
@@ -644,7 +644,7 @@ function createLantern() {
 /* Start lanterns */
 function startLanterns() {
   stopLanterns();
-  lanternInterval = setInterval(createLantern, 1100);
+  lanternInterval = setInterval(createLantern, 1000);
 }
 
 /* Stop lanterns */
